16c34ffea481cfdb78563355b7dedd1361988116
Kagan Sari <kagan.sari@getir.com>
2016-04-27 01:59:56 +0300

    unused codes removed

--- WebApplication/test/TestModelTest.java
+++ WebApplication/test/TestModelTest.java
@@ -4,36 +4,16 @@
  * and open the template in the editor.
  */
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author tv
+ * @author Kagan Sari
  */
 public class TestModelTest {
     
     public TestModelTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
     }
 
     /**
