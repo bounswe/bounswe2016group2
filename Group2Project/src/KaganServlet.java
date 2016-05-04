c7152fd13e4b45ca7d21e03faa744c4c1b1393af
Kagan Sari <kagan.sari@getir.com>
2016-05-04 23:26:58 +0300

    SPARQL query added for semantic tag fetching

--- Group2Project/src/KaganServlet.java
+++ Group2Project/src/KaganServlet.java
@@ -1,11 +1,33 @@
 
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+//import org.apache.jena.query.Dataset;
+//import org.apache.jena.query.DatasetFactory;
+//import org.apache.jena.query.Query;
+//import org.apache.jena.query.QueryExecution;
+//import org.apache.jena.query.QueryExecutionFactory;
+//import org.apache.jena.query.QueryFactory;
+//import org.apache.jena.query.QuerySolution;
+//import org.apache.jena.query.ResultSet;
+//import org.apache.jena.sparql.function.library.print;
+//import org.json.simple.JSONArray;
+import org.apache.commons.io.IOUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * Servlet implementation class KaganServlet
@@ -19,22 +41,72 @@ public class KaganServlet extends HttpServlet {
      */
     public KaganServlet() {
         super();
-        // TODO Auto-generated constructor stub
+    }
+
+    protected String getQueryString() throws UnsupportedEncodingException {
+		String queryStr =
+			"PREFIX wd: <http://www.wikidata.org/entity/> " +
+			"PREFIX wdt: <http://www.wikidata.org/prop/direct/> " +
+			"PREFIX wikibase: <http://wikiba.se/ontology#> " +
+			"PREFIX p: <http://www.wikidata.org/prop/> " +
+			"PREFIX ps: <http://www.wikidata.org/prop/statement/> " +
+			"PREFIX pq: <http://www.wikidata.org/prop/qualifier/> " +
+			"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+			"PREFIX bd: <http://www.bigdata.com/rdf#>" +
+			"PREFIX ogc: <http://www.opengis.net/ont/geosparql#>" +
+			"SELECT DISTINCT ?place ?placeLabel ?gps " +
+			"WHERE " +
+			"{ " +
+			"  SERVICE wikibase:around {" +
+			"    ?place wdt:P625 ?gps . " +
+			"    bd:serviceParam wikibase:center \"Point(29.051115 41.084458)\"^^ogc:wktLiteral . " +
+			"    bd:serviceParam wikibase:radius \"1\" . " +
+			"  }" +
+			"  SERVICE wikibase:label {" +
+			"    bd:serviceParam wikibase:language \"en\" ." +
+			"  }" +
+			"}" +
+			"LIMIT 10";
+		return URLEncoder.encode(queryStr, "UTF-8");
+	}
+    
+    protected JSONObject getItems() throws IOException, ParseException {
+    	String queryStr = getQueryString();
+    	URL path = new URL("https://query.wikidata.org/sparql?format=json&query=" + queryStr);
+    	URLConnection con = path.openConnection();
+
+    	InputStream in = con.getInputStream();
+    	String encoding = con.getContentEncoding();
+    	encoding = encoding == null ? "UTF-8" : encoding;
+    	String body = IOUtils.toString(in, encoding);
+
+    	JSONParser parser = new JSONParser();
+    	JSONObject data = (JSONObject) parser.parse(body);
+		return data;
+    	// Query query = QueryFactory.create(queryStr);
+		// Dataset model = DatasetFactory.create();
+		// QueryExecution qExe = QueryExecutionFactory.create(query, model);
+		// ResultSet resultSet = qExe.execSelect();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter(); 
+		try {
+			JSONObject items = getItems();
+			out.println(items);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- Group2Project/src/MainPageServlet.java
+++ Group2Project/src/MainPageServlet.java
@@ -48,7 +48,7 @@ public class MainPageServlet extends HttpServlet {
 							+ "<td> <a href=\"https://github.com/bounswe/bounswe2016group2/wiki/Yiğit-Özgümüş\"> Git Profile Page </a> </td>"
 						+ "</tr>"
 						+ "<tr>"
-							+ "<td> <a href=\"YigitOzgumus\"> Kagan Sari </a> </td>"
+							+ "<td> <a href=\"KaganSari\"> Kagan Sari </a> </td>"
 							+ "<td> <a href=\"https://github.com/bounswe/bounswe2016group2/wiki/Yiğit-Özgümüş\"> Git Profile Page </a> </td>"
 						+ "</tr>"
 						+ "<tr>"
