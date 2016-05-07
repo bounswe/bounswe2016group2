b928d15a576a6cb30061190c3dc4099ed6b61a11
enesozi <enesozi@users.noreply.github.com>
2016-05-07 11:38:01 +0300

    Added files via upload

--- Group2Project/src/EnesServlet.java
+++ Group2Project/src/EnesServlet.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class EnesServlet
  */
-@WebServlet("/EnesServlet")
+@WebServlet("/EnesOzipek")
 public class EnesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +29,8 @@ public class EnesServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		out.println("This is Enes' page");
 	}
 
 	/**
