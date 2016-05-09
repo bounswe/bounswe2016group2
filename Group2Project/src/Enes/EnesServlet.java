237d3d36989fe255230c8c2ad2a1eb1295a5bafe
ozipekenes <enes_ozipek94@hotmail.com>
2016-05-10 00:35:43 +0300

    Query can be modified. Checkboxes are handled.

--- Group2Project/src/Enes/EnesServlet.java
+++ Group2Project/src/Enes/EnesServlet.java
@@ -8,7 +8,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,8 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.jena.query.QuerySolution;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.impl.LiteralImpl;
 
 /**
  * Servlet implementation class Yunus connects to database
@@ -39,6 +36,12 @@ public class EnesServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
         PrintWriter out = response.getWriter(); /**< Our writer to type html codes */
+         if(request.getParameter("Save")!=null){
+			String [] selectedRows = request.getParameterValues("labels");
+			for (String str : selectedRows) {
+				System.out.println(str);
+			}
+		}
         out.println("<html>");
         
 		Db dao = new Db("group2Project", "root", "1234");
@@ -55,39 +58,42 @@ public class EnesServlet extends HttpServlet {
 			Statement st = connection.createStatement();
 			DatabaseMetaData dbm = connection.getMetaData();
 			// check if "employee" table is there
-			ResultSet tables = dbm.getTables(null, null, "TORTURE", null);
+			ResultSet tables = dbm.getTables(null, null, "LargestCities", null);
+			
 			if (tables.next()) {
-			  // Table exists
+				st.executeUpdate("DROP table LargestCities");
+			  System.out.println("Hayda");
+			  
 			}
 			else {
-				st.executeUpdate(
-				        "CREATE TABLE IF NOT EXISTS Torture ("
+				
+				 st.executeUpdate(
+				        "CREATE TABLE IF NOT EXISTS LargestCities ("
 				        + " id INTEGER PRIMARY KEY AUTO_INCREMENT,"
-				        + " nadress VARCHAR(255) NOT NULL,"
-				        + " label VARCHAR(255),"
-				        + " imageL  VARCHAR(255))");
+				        + " cname VARCHAR(255) NOT NULL,"
+				        + " population VARCHAR(255),"
+				        + " gps  VARCHAR(255))");
+				
+					System.out.println("Heyyo");
 				Sparql sparql = new Sparql();
 		    	org.apache.jena.query.ResultSet res = sparql.getQueryResults();
-		    	while (res.hasNext())
+		   	/*while (res.hasNext())
 		    	{
 		    		QuerySolution binding = res.nextSolution();
-		    		String itm = binding.get("thing").toString();
-		    		LiteralImpl itm2 = (LiteralImpl) binding.getLiteral("thingLabel");
-		    		String s = itm2.toString();
-		    		String itm3 =  binding.get("image").toString();
-		    		String query = " INSERT into Torture (nadress, label, imageL)"+" values (?, ?, ?)";
-		    		
+		    		String itm =  binding.get("cityLabel").toString();
+		    		String itm2 =  binding.get("population").toString();
+		    		String itm3 =  binding.get("gps").toString();
+		    		String query = " INSERT into LargestCities (cname, population, gps)"+" values (?, ?, ?)";
 		    		      // create the mysql insert preparedstatement
 		    		      PreparedStatement preparedStmt = connection.prepareStatement(query);
-		    		      preparedStmt.setString (1, itm);
-		    		      preparedStmt.setString (2, s.substring(0,s.length()-3));
-		    		      preparedStmt.setString(3, itm3);		    		
+		    		      preparedStmt.setString(1, itm.substring(0,itm.indexOf("@")));
+		    		      preparedStmt.setString(2, itm2.substring(0,itm2.indexOf("^")));
+		    		      preparedStmt.setString(3, itm3.substring(0,itm3.indexOf("^")));		    		
 		    		      // execute the preparedstatement
 		    		      preparedStmt.execute();
-		    	}
+		    	}*/
 			}
 					connection.close();
-			
 		} catch (SQLException e) {
 			out.print("NOPE </br>");
 			e.printStackTrace();
@@ -96,7 +102,7 @@ public class EnesServlet extends HttpServlet {
         out.println();
         out.println("<form name=\"loginForm\" method=\"post\" action=\"Enes\">");
         out.println("Search: <input type=\"text\" name=\"searchText\"/> <br/>");
-        out.println("<input type=\"submit\" value=\"Search\" />");
+        out.println("<input type=\"submit\" name=\"Search\" value=\"Search\" />");
         out.println("</form>");
        
     	out.println("</html>");
@@ -105,40 +111,57 @@ public class EnesServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		if(request.getParameter("searchText")!=null){
+		if(request.getParameter("Search")!=null){
 			String text = request.getParameter("searchText");
 			 PrintWriter writer = response.getWriter();
 	           Db dao = new Db("group2Project", "root", "1234");
 	   		   Connection connection = dao.getConnection(); /**< database connection variable*/
-	   		   Statement st;
+	   		   
 			try {
-				st = connection.createStatement();
-				ResultSet rs = st.executeQuery("SELECT * from Torture");
+				Statement st = connection.createStatement();
+				st.executeUpdate(
+				        "CREATE TABLE IF NOT EXISTS LargestCities ("
+				        + " id INTEGER PRIMARY KEY AUTO_INCREMENT,"
+				        + " cname VARCHAR(255) NOT NULL,"
+				        + " population VARCHAR(255),"
+				        + " gps  VARCHAR(255))");
 	            writer.println("<html>");
 	            writer.println("<body>");
 		        writer.println("<form action=\"Enes\" method=\"GET\"> ");
 	            writer.println("<table style=\"width:100%\">");
 	            writer.println("<tr>");
-	            writer.println("<th>"+"Label"+"</th>");
-	            writer.println("<th>"+"LabelThing"+"</th>");
-	            writer.println("<th>"+"Image"+"</th>");
+	            writer.println("<th>"+"CNAME"+"</th>");
+	            writer.println("<th>"+"POPULATION"+"</th>");
+	            writer.println("<th>"+"GPS"+"</th>");
 	            writer.println("<th>Save</th>");
 	            writer.println("</tr>");
-	            while (rs.next())
-	        	{
-	            	int id = rs.getInt("ID");
-	            	String nadress = rs.getString("NADRESS");
-	            	String label = rs.getString("LABEL");
-	            	String image = rs.getString("IMAGEL");
-	        		writer.println("<tr>");
-	        		writer.println("<td>"+nadress+"</td>");
-	        		writer.println("<td>"+label+"</td>");
-	        		writer.println("<td>"+image+"</td>");
-	        		writer.println("<td><input type=\"checkbox\" name=\"labels\""+"value=\""+id+"\"/></td>");
-	                writer.println("</tr>"); 
-	        	}
+	            Sparql sparql = new Sparql(text);
+		    	org.apache.jena.query.ResultSet res = sparql.getQueryResults();
+		    	int id=1;
+		    	while (res.hasNext())
+		    	{
+		    		QuerySolution binding = res.nextSolution();
+		    		String itm =  binding.get("cityLabel").toString();
+		    		String itm2 =  binding.get("population").toString();
+		    		String itm3 =  binding.get("gps").toString();
+		    		String query = " INSERT into LargestCities (cname, population, gps)"+" values (?, ?, ?)";
+		    		      // create the mysql insert preparedstatement
+		    		      PreparedStatement preparedStmt = connection.prepareStatement(query);
+		    		      preparedStmt.setString(1, itm.substring(0,itm.indexOf("@")));
+		    		      preparedStmt.setString(2, itm2.substring(0,itm2.indexOf("^")));
+		    		      preparedStmt.setString(3, itm3.substring(0,itm3.indexOf("^")));		    		
+		    		      // execute the preparedstatement
+		    		      preparedStmt.execute();
+		    		      writer.println("<tr>");
+			        		writer.println("<td>"+itm.substring(0,itm.indexOf("@"))+"</td>");
+			        		writer.println("<td>"+itm2.substring(0,itm2.indexOf("^"))+"</td>");
+			        		writer.println("<td>"+itm3.substring(0,itm3.indexOf("^"))+"</td>");
+			        		writer.println("<td><input type=\"checkbox\" name=\"labels\""+"value=\""+id+"\"/></td>");
+			                writer.println("</tr>"); 
+		    		      id++;
+		    	}
 	            writer.println("</table>");
-	            writer.println("<input type=\"submit\" value=\"Save\" />");
+	            writer.println("<input type=\"submit\" name=\"Save\" value=\"Save\" />");
 	            writer.println("</form>");
 	            writer.println("</body>");
 	            writer.println("</html>");
@@ -146,9 +169,12 @@ public class EnesServlet extends HttpServlet {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else if(request.getParameter("Save")!=null){
-			
-		}else{
+		}/*else if(request.getParameter("Save")!=null){
+			String [] selectedRows = request.getParameterValues("labels");
+			for (String str : selectedRows) {
+				System.out.println(str);
+			}
+		}*/else{
 			doGet(request, response);
 		}
 	}
--- Group2Project/src/Enes/Sparql.java
+++ Group2Project/src/Enes/Sparql.java
@@ -8,44 +8,50 @@ import org.apache.jena.query.ResultSet;
 
 public class Sparql {
 	private ResultSet results;
-	private String squery = "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
-            "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
-            "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
-            "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
-            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
-			"SELECT ?thing ?thingLabel ?image\n"+
-			"WHERE\n"+
-			"{\n"+
-				"?thing wdt:P366 wd:Q132781 .\n"+
-				"?thing wdt:P18 ?image .\n"+
-				"?s wdt:P50 ?author .\n"+
-				"SERVICE wikibase:label {\n"+
-				"bd:serviceParam wikibase:language \"en\" \n"+
-				"}\n"+
-			"}\n LIMIT 10";
+	private String squery = "";
 				
-
 	public Sparql() {
 		squery = "PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
 	            "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
 	            "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
 	            "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
 	            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
-				"SELECT ?thing ?thingLabel ?image\n"+
+	            "PREFIX bd: <http://www.bigdata.com/rdf#>\n"+
+				"SELECT DISTINCT ?cityLabel ?population ?gps\n"+
 				"WHERE\n"+
 				"{\n"+
-					"?thing wdt:P366 wd:Q132781 .\n"+
-					"?thing wdt:P18 ?image .\n"+
-					"?s wdt:P50 ?author .\n"+
+					"?city wdt:P31/wdt:P279* wd:Q515 .\n"+
+					"?city wdt:P1082 ?population .\n"+
+					"?city wdt:P625 ?gps .\n"+
 					"SERVICE wikibase:label {\n"+
 					"bd:serviceParam wikibase:language \"en\" \n"+
 					"}\n"+
-				"}\n LIMIT 100";
+				"}\n ORDER BY DESC(?population) LIMIT 100";
         Query query = QueryFactory.create(squery); 
         QueryExecution qExe = QueryExecutionFactory.sparqlService( "https://query.wikidata.org/sparql", query );
         results = qExe.execSelect();
 	}
-
+	public Sparql(String text){
+		squery = "PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
+	            "PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+	            "PREFIX wd: <http://www.wikidata.org/entity/>\n" +
+	            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+	            "PREFIX bd: <http://www.bigdata.com/rdf#>\n"+
+				"SELECT DISTINCT ?cityLabel ?population ?gps\n"+
+				"WHERE\n"+
+				"{\n"+
+					"?city wdt:P31/wdt:P279* wd:Q515 .\n"+
+					"?city wdt:P1082 ?population .\n"+
+					"?city wdt:P625 ?gps .\n"+
+					"FILTER(?population >= "+text+").\n"+
+					"SERVICE wikibase:label {\n"+
+					"bd:serviceParam wikibase:language \"en\" .\n"+
+					"}\n"+
+				"}\n ORDER BY DESC(?population) LIMIT 100";
+        Query query = QueryFactory.create(squery); 
+        QueryExecution qExe = QueryExecutionFactory.sparqlService( "https://query.wikidata.org/sparql", query );
+        results = qExe.execSelect();
+	}
 	public ResultSet getQueryResults() {
 		return results;
 	}
