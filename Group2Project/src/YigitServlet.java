af274871e30b46ee41ef8590dd77262e69468d5f
Yigit Ozgumus <yigitozgumus1@gmail.com>
2016-05-10 11:26:29 +0300

    query string and database connection check

--- Group2Project/src/YigitServlet.java
+++ Group2Project/src/YigitServlet.java
@@ -2,6 +2,11 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/YigitOzgumus")
 public class YigitServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	int duration = 0;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -22,6 +28,53 @@ public class YigitServlet extends HttpServlet {
     public YigitServlet() {
         super();
         // TODO Auto-generated constructor stub
+    }
+    /** Creates the database connection using the credentials defined in mainPageServlet.
+     * 	Checks the class and the SQL availability
+     * @return The database connection object
+     */
+    protected Connection establishDatabase(){
+    	try{
+    		Class.forName("com.mysql.jdbc.Driver");
+    	}catch(ClassNotFoundException e1){
+    		e1.printStackTrace();
+    	}
+    	Connection conn;
+    	try{
+    		conn = DriverManager.getConnection(MainPageServlet.DB_URL,MainPageServlet.DB_USER,MainPageServlet.DB_PASSWORD);
+    		return conn;
+    	}catch(SQLException e){
+    		e.printStackTrace();
+    	}
+    	return null;
+    }
+    /**
+     *  Store the main query String for modularity
+     * @return the query string with the right encoding
+     * @throws UnsupportedEncodingException
+     */
+    protected String getQuery() throws UnsupportedEncodingException{
+    	String query = "PREFIX wd: <http://www.wikidata.org/entity/> " +
+    				   "PREFIX wdt: <http://www.wikidata.org/prop/direct/> " +
+    				   "PREFIX wikibase: <http://wikiba.se/ontology#> " +
+    				   "PREFIX p: <http://www.wikidata.org/prop/> " +
+    				   "PREFIX ps: <http://www.wikidata.org/prop/statement/> " +
+    				   "PREFIX pq: <http://www.wikidata.org/prop/qualifier/> " +
+    				   "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+    				   "PREFIX bd: <http://www.bigdata.com/rdf#> " +
+    				   "SELECT ?Film ?FilmLabel ?Duration ?ComposerLabel ?genreLabel ?dateLabel " +
+    				   "WHERE { " +
+    				   "?Film wdt:P31 wd:Q11424. " +
+    				   "?Film wdt:P2047 ?Duration. " +
+    				   "?Film wdt:P86 ?Composer. " +
+    				   "?Film wdt:P136  ?genre. " +
+    				   "OPTIONAL { ?Film wdt:P577 ?dateLabel. } " +
+    				   "SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". } " +
+    				   "FILTER(?Duration >= 100). " +
+    				   "} " +
+    				   "LIMIT 50 " ;
+    	return URLEncoder.encode(query,"UTF-8");
+
     }
 
 	/**
