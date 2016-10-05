f559b4d987b3418f6e4f3b27dc49e850bb67046e
kagansari <kagannsari@gmail.com>
2016-10-05 23:55:27 +0300

    database set to mysql (with trivial credentials to be modified)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bounswe2016group2',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '1123'
     }
 }
 
