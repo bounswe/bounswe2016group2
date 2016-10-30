4d9d34341d1b2247d0ee6c80e355a3d821db6390
ozipekenes <enes_ozipek94@hotmail.com>
2016-10-30 19:42:23 +0300

    Retrofit was not tested

---
+++ android/EatRight/app/src/main/java/com/example/bounswegroup2/Models/MicroNutrient.java
@@ -0,0 +1,8 @@
+package com.example.bounswegroup2.Models;
+
+/**
+ * Created by Enes on 30.10.2016.
+ */
+
+public class MicroNutrient {
+}
--- android/EatRight/app/src/main/java/com/example/bounswegroup2/Utils/API.java
+++ android/EatRight/app/src/main/java/com/example/bounswegroup2/Utils/API.java
@@ -3,13 +3,14 @@ package com.example.bounswegroup2.Utils;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.example.bounswegroup2.Utils.Constants.BASE_URL;
+
 /**
  * This class is to utilize retrofit instance
  * Created by Enes on 21.10.2016.
  */
 
 public class API {
-    public static final String BASE_URL = "";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
