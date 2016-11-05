01def14440d40127bce3a7038cb8e891cb2240d6
Kagan Sari <kagannsari@gmail.com>
2016-11-05 07:09:14 +0200

    More flake8

--- web/api/service/micronutrient.py
+++ web/api/service/micronutrient.py
@@ -3,13 +3,16 @@ from django.shortcuts import get_object_or_404
 from api.models import Micronutrient
 import api.mock.nutrient as MicronutrientMock
 
+
 def all():
     return Micronutrient.objects.values()
+
 
 def create(name):
     micro = Micronutrient(name=name)
     micro.save()
     return micro
+
 
 def createDefaults():
     allNames = MicronutrientMock.getAllNames()
@@ -19,11 +22,10 @@ def createDefaults():
         except Exception:
             print(name, ' already exists')
 
-def deleteById(id):
-    get_object_or_404(Micronutrient, id=id).delete()
 
 def deleteBySlug(slug):
     get_object_or_404(Micronutrient, slug=slug).delete()
+
 
 def deleteAll():
     Micronutrient.objects.all().delete()
