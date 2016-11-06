ba4af6a203e990a11cc2275fd2d4495fb1ae1a62
Kagan Sari <kagannsari@gmail.com>
2016-11-06 23:36:18 +0200

    user components moved into separate files

---
+++ web/web/app/components/signInModal.js
@@ -0,0 +1,61 @@
+class SignInModal extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {}
+    this.emailChanged = this.emailChanged.bind(this)
+    this.passwordChanged = this.passwordChanged.bind(this)
+    this.submit = this.submit.bind(this)
+  }
+
+  emailChanged(e) {this.setState({email: e.target.value}) }
+  passwordChanged(e) {this.setState({password: e.target.value}) }
+
+  submit(e) {
+    e.preventDefault()
+    this.setState({error: false})
+    const postData = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    Api.signin(postData)
+      .then((data) => {
+        reactCookie.save('token', data.token)
+        reactCookie.save('email', postData.email)
+        window.location.href = '/'
+      }).catch((err) => {
+        this.setState({error: true})
+      })
+  }
+
+  render() {
+    let formClassName = 'ui form'
+    if (this.state.error) formClassName += ' error'
+    return (
+      <div id='signInModal' className="ui small modal">
+        <i className="close icon"></i>
+        <div className="header">
+          Sign In
+        </div>
+        <div className="content">
+          <form className={formClassName}>
+            <div className="field">
+              <label>E-mail</label>
+              <input type="text" name="email" placeholder="E-mail" value={this.state.email} onChange={this.emailChanged}/>
+            </div>
+            <div className="field">
+              <label>Password</label>
+              <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.passwordChanged}/>
+            </div>
+            <div className="ui error message">
+              <p>Invalid email or password</p>
+            </div>
+            <button className="ui button" type="submit" style={{width:'100%'}} onClick={this.submit}>
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
--- web/web/static/components/signUpModal.js
+++ web/web/static/components/signUpModal.js
@@ -8,131 +8,24 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var SignInModal = function (_React$Component) {
-  _inherits(SignInModal, _React$Component);
-
-  function SignInModal(props) {
-    _classCallCheck(this, SignInModal);
-
-    var _this = _possibleConstructorReturn(this, (SignInModal.__proto__ || Object.getPrototypeOf(SignInModal)).call(this, props));
-
-    _this.state = {};
-    _this.emailChanged = _this.emailChanged.bind(_this);
-    _this.passwordChanged = _this.passwordChanged.bind(_this);
-    _this.submit = _this.submit.bind(_this);
-    return _this;
-  }
-
-  _createClass(SignInModal, [{
-    key: 'emailChanged',
-    value: function emailChanged(e) {
-      this.setState({ email: e.target.value });
-    }
-  }, {
-    key: 'passwordChanged',
-    value: function passwordChanged(e) {
-      this.setState({ password: e.target.value });
-    }
-  }, {
-    key: 'submit',
-    value: function submit(e) {
-      var _this2 = this;
-
-      e.preventDefault();
-      this.setState({ error: false });
-      var postData = {
-        email: this.state.email,
-        password: this.state.password
-      };
-      Api.signin(postData).then(function (data) {
-        reactCookie.save('token', data.token);
-        reactCookie.save('email', postData.email);
-        window.location.href = '/';
-      }).catch(function (err) {
-        _this2.setState({ error: true });
-      });
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      var formClassName = 'ui form';
-      if (this.state.error) formClassName += ' error';
-      return React.createElement(
-        'div',
-        { id: 'signInModal', className: 'ui small modal' },
-        React.createElement('i', { className: 'close icon' }),
-        React.createElement(
-          'div',
-          { className: 'header' },
-          'Sign In'
-        ),
-        React.createElement(
-          'div',
-          { className: 'content' },
-          React.createElement(
-            'form',
-            { className: formClassName },
-            React.createElement(
-              'div',
-              { className: 'field' },
-              React.createElement(
-                'label',
-                null,
-                'E-mail'
-              ),
-              React.createElement('input', { type: 'text', name: 'email', placeholder: 'E-mail', value: this.state.email, onChange: this.emailChanged })
-            ),
-            React.createElement(
-              'div',
-              { className: 'field' },
-              React.createElement(
-                'label',
-                null,
-                'Password'
-              ),
-              React.createElement('input', { type: 'password', name: 'password', placeholder: 'Password', value: this.state.password, onChange: this.passwordChanged })
-            ),
-            React.createElement(
-              'div',
-              { className: 'ui error message' },
-              React.createElement(
-                'p',
-                null,
-                'Invalid email or password'
-              )
-            ),
-            React.createElement(
-              'button',
-              { className: 'ui button', type: 'submit', style: { width: '100%' }, onClick: this.submit },
-              'Submit'
-            )
-          )
-        )
-      );
-    }
-  }]);
-
-  return SignInModal;
-}(React.Component);
-
-var SignUpModal = function (_React$Component2) {
-  _inherits(SignUpModal, _React$Component2);
+var SignUpModal = function (_React$Component) {
+  _inherits(SignUpModal, _React$Component);
 
   function SignUpModal(props) {
     _classCallCheck(this, SignUpModal);
 
-    var _this3 = _possibleConstructorReturn(this, (SignUpModal.__proto__ || Object.getPrototypeOf(SignUpModal)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (SignUpModal.__proto__ || Object.getPrototypeOf(SignUpModal)).call(this, props));
 
-    _this3.state = {
+    _this.state = {
       data: null,
       errors: null
     };
-    _this3.first_nameChanged = _this3.first_nameChanged.bind(_this3);
-    _this3.last_nameChanged = _this3.last_nameChanged.bind(_this3);
-    _this3.emailChanged = _this3.emailChanged.bind(_this3);
-    _this3.passwordChanged = _this3.passwordChanged.bind(_this3);
-    _this3.submit = _this3.submit.bind(_this3);
-    return _this3;
+    _this.first_nameChanged = _this.first_nameChanged.bind(_this);
+    _this.last_nameChanged = _this.last_nameChanged.bind(_this);
+    _this.emailChanged = _this.emailChanged.bind(_this);
+    _this.passwordChanged = _this.passwordChanged.bind(_this);
+    _this.submit = _this.submit.bind(_this);
+    return _this;
   }
 
   _createClass(SignUpModal, [{
@@ -158,7 +51,7 @@ var SignUpModal = function (_React$Component2) {
   }, {
     key: 'submit',
     value: function submit(e) {
-      var _this4 = this;
+      var _this2 = this;
 
       e.preventDefault();
       this.setState({ errors: null });
@@ -175,7 +68,7 @@ var SignUpModal = function (_React$Component2) {
           window.location.href = '/';
         });
       }).catch(function (err) {
-        _this4.setState({ errors: err.data });
+        _this2.setState({ errors: err.data });
       });
     }
   }, {
@@ -278,78 +171,4 @@ var SignUpModal = function (_React$Component2) {
   }]);
 
   return SignUpModal;
-}(React.Component);
-
-var NavbarUser = function (_React$Component3) {
-  _inherits(NavbarUser, _React$Component3);
-
-  function NavbarUser(props) {
-    _classCallCheck(this, NavbarUser);
-
-    var _this5 = _possibleConstructorReturn(this, (NavbarUser.__proto__ || Object.getPrototypeOf(NavbarUser)).call(this, props));
-
-    _this5.state = { userEmail: props.userEmail };
-    _this5.openSigninModal = _this5.openSigninModal.bind(_this5);
-    _this5.openSignupModal = _this5.openSignupModal.bind(_this5);
-    _this5.signout = _this5.signout.bind(_this5);
-    return _this5;
-  }
-
-  _createClass(NavbarUser, [{
-    key: 'openSigninModal',
-    value: function openSigninModal() {
-      $('#signInModal.ui.modal').modal('show');
-    }
-  }, {
-    key: 'openSignupModal',
-    value: function openSignupModal() {
-      $('#signUpModal.ui.modal').modal('show');
-    }
-  }, {
-    key: 'signout',
-    value: function signout() {
-      reactCookie.remove('email');
-      reactCookie.remove('token');
-      window.location.href = '/';
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      if (this.state.userEmail) {
-        return React.createElement(
-          'div',
-          { className: 'right menu' },
-          React.createElement(
-            'a',
-            { className: 'item' },
-            userEmail
-          ),
-          React.createElement(
-            'a',
-            { className: 'item', onClick: this.signout },
-            'Sign Out'
-          )
-        );
-      } else {
-        return React.createElement(
-          'div',
-          { className: 'right menu' },
-          React.createElement(SignInModal, null),
-          React.createElement(
-            'a',
-            { className: 'item', onClick: this.openSigninModal },
-            'Sign In'
-          ),
-          React.createElement(SignUpModal, null),
-          React.createElement(
-            'a',
-            { className: 'item', onClick: this.openSignupModal },
-            'Sign Up'
-          )
-        );
-      }
-    }
-  }]);
-
-  return NavbarUser;
 }(React.Component);
