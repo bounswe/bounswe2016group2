bf30c780c107123ce019af858785283b43452281
gozdeberk <gozdeberk1@gmail.com>
2016-11-12 04:30:54 +0200

    Layout of the food page

---
+++ android/EatRight/app/src/main/java/com/example/bounswegroup2/eatright/FoodPageActivity.java
@@ -0,0 +1,13 @@
+package com.example.bounswegroup2.eatright;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_page);
+    }
+}
