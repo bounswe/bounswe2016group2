de0d52a5d8b123bd30865f52c23056bf41ccd657
gozdeberk <gozdeberk1@gmail.com>
2016-11-23 11:47:56 +0200

    Popup window is created to see ingredients of the food via "Ingredients" button in the food page.

--- android/EatRight/app/src/main/java/com/example/bounswegroup2/eatright/FoodPageActivity.java
+++ android/EatRight/app/src/main/java/com/example/bounswegroup2/eatright/FoodPageActivity.java
@@ -1,9 +1,18 @@
 package com.example.bounswegroup2.eatright;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.PopupWindow;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 import com.example.bounswegroup2.Models.Food;
 import com.example.bounswegroup2.Models.Ingredient;
@@ -22,6 +31,8 @@ public class FoodPageActivity extends AppCompatActivity {
     private Integer pro;
     private Integer carb;
     private Integer fat;
+    Button btnOpenIngredients;
+    Button btnCloseIngredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,5 +69,39 @@ public class FoodPageActivity extends AppCompatActivity {
 
         totalProtein = (TextView) findViewById(R.id.protein_result_text);
         totalProtein.setText(pro.toString());
+
+        btnOpenIngredients = (Button) findViewById(R.id.ingredients_button);
+        btnOpenIngredients.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v){
+                initiatePopupWindow();
+            }
+        });
     }
+    private PopupWindow popupIngredients;
+
+    private void initiatePopupWindow() {
+        try {
+            // We need to get the instance of the LayoutInflater
+            LayoutInflater inflater = (LayoutInflater) FoodPageActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.ingredients_popup,
+                    (ViewGroup) findViewById(R.id.popup_element));
+            popupIngredients = new PopupWindow(layout, 300, 370, true);
+            popupIngredients.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+            btnCloseIngredients = (Button) layout.findViewById(R.id.btn_close_ingredients_popup);
+            btnCloseIngredients.setOnClickListener(cancel_button_click_listener);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private OnClickListener cancel_button_click_listener = new OnClickListener() {
+        public void onClick(View v) {
+            popupIngredients.dismiss();
+
+        }
+    };
+
 }
