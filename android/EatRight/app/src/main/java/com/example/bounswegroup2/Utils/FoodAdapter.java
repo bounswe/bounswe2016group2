69955540c5899db1005ec83bb01bb360b4379e78
Yigit Ozgumus <yigitozgumus1@gmail.com>
2016-11-23 20:19:40 +0300

    Food Adapter implementation

---
+++ android/EatRight/app/src/main/java/com/example/bounswegroup2/Utils/FoodAdapter.java
@@ -0,0 +1,76 @@
+package com.example.bounswegroup2.Utils;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.bounswegroup2.eatright.R;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by yigitozgumus on 11/23/16.
+ */
+
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
+
+
+    private static ClickListener clickListener;
+
+    //Creating onClick listener for the ImageTexts
+    public interface ClickListener {
+        void onItemClick(int position, View v);
+        void onItemLongClick(int position, View v);
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener, View.OnLongClickListener {
+        ImageView foodImage;
+        public ViewHolder(View view) {
+            super(view);
+            foodImage = (ImageView)view.findViewById(R.id.food_image);
+            foodImage.setOnClickListener(this);
+            foodImage.setOnLongClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            clickListener.onItemClick(getAdapterPosition(), v);
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            clickListener.onItemLongClick(getAdapterPosition(), v);
+            return false;
+        }
+
+    }
+
+    public void setOnItemClickListener(ClickListener clickListener) {
+        FoodAdapter.clickListener = clickListener;
+    }
+
+    @Override
+    public FoodAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.food_card,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(FoodAdapter.ViewHolder holder, int position) {
+//        Picasso.with(context)
+//                .load(artCollections.get(position).getWebImageUrl())
+//                .fit()
+//                .placeholder(R.drawable.rijksmuseum)
+//                .into(holder.imgCollection);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
