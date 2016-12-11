a033645d1efb1e5cc5b581fbf8ad98cc62cdcfa6
gozdeberk <gozdeberk1@gmail.com>
2016-12-11 16:47:07 +0200

    Server page initial layout

---
+++ android/EatRight/app/src/main/java/com/example/bounswegroup2/eatright/ServerPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.bounswegroup2.eatright;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ServerPageActivity extends AppCompatActivity {
+
+    private TextView serverName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_page);
+    }
+}
