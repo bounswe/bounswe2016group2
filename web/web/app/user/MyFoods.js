4e7260a356dde7ac52c6a79bb6904a51a7ef2eff
Arda Yoruk <yoruk_arda@hotmail.com>
2016-12-16 17:44:24 +0300

    Empty components for my foods and my restaurants generated

---
+++ web/web/app/user/MyFoods.js
@@ -0,0 +1,15 @@
+export default class MyFoods extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      foods: props.foods
+    }
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
---
+++ web/web/app/user/MyRestaurants.js
@@ -0,0 +1,15 @@
+export default class MyRestaurants extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      restaurants: props.restaurants
+    }
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
